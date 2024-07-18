4a3ad267e4841ab0e4e2acf25235445a59dab514
polizogo <polizogopoulos@iridalabs.com>
2024-07-18 10:00:54 +0300

    Deployment with docker
    - docker commands
    - docker compose for connector

---
+++ docker/run_connector.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run -d \
+    --sysctl net.ipv4.ping_group_range="0 2147483647" \
+    --env TWINGATE_NETWORK="<network name>" \
+    --env TWINGATE_ACCESS_TOKEN="<>" \
+    --env TWINGATE_REFRESH_TOKEN="<>"  \
+    --env TWINGATE_LABEL_HOSTNAME="`hostname`" \
+    --env TWINGATE_LABEL_DEPLOYED_BY="docker" \
+    --name "twingate-<connector name>" \
+    --restart=unless-stopped \
+    --pull=always \
+    twingate/connector:1
