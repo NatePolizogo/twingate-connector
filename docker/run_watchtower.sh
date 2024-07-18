7c08d2ac93c3745acd972fcf2835b0a589d13bb7
polizogo <polizogopoulos@iridalabs.com>
2024-07-18 10:05:55 +0300

    added watchtower to docker compose

--- docker/run_watchtower.sh
+++ docker/run_watchtower.sh
@@ -3,5 +3,5 @@
 docker run -d \
     --name watchtower \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    --label-enable=true \
-    containrrr/watchtower
+    containrrr/watchtower \
+    --label-enable=true
